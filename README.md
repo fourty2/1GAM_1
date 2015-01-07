d5a5a8c511a03e5977f12fadf3b26afa4b658de0
Marc Henklein <snooze82@gmail.com>
2015-01-07 21:44:21 +0100

    initial commit, bouncy test :)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # 1GAM_1
 January entry for #1GAM challenge
+
+Should become a 2P Beat'em'Up, 3D, low poly, webgl.
+I'd like adorable little bouncy creatures.
+
+Tools:
+	* Blender
+	* GIMP
+	* THREE.js (r68 -- r69 had some problems with my models)
+	* Sublime Text
---
+++ js/game.js
@@ -0,0 +1,77 @@
+var game = {
+	gamecanvas: null,
+	renderer: null,
+	scene: null,
+	camera: null,
+	cameraControls: null,
+
+	velocityY: 0,
+
+	renderer: null,
+	init: function() {
+		var WIDTH = window.innerWidth;
+		var HEIGHT = window.innerHeight;
+		var NEAR = 0.1;
+		var FAR = 1000;
+		var FOV = 45; 
+
+		this.gamecanvas = document.getElementById('gamecanvas');
+		this.renderer = new THREE.WebGLRenderer({canvas: gamecanvas});
+
+    	this.renderer.setSize(WIDTH, HEIGHT);
+		this.renderer.shadowMapEnabled = true;
+		this.renderer.shadowMapSoft = true;		
+
+		this.scene = new THREE.Scene();
+
+		this.camera = new THREE.PerspectiveCamera(FOV, WIDTH/ HEIGHT, NEAR, FAR);
+	    this.camera.position.set(0, 60, -300 );
+
+	    this.scene.add(this.camera);
+
+	    this.buildWorld();
+	    this.animate();
+
+
+	},
+	buildWorld: function() {
+		// plane
+		this.plane = new THREE.Mesh(
+				new THREE.PlaneGeometry(60,60,8,8),
+				new THREE.MeshBasicMaterial({color: 0xff0000, side: THREE.DoubleSide})
+			);
+		this.plane.rotateX(Math.PI/2);
+		this.plane.position.set(0,-50,0);
+		this.scene.add(this.plane);
+
+		this.player = new THREE.Mesh(
+				new THREE.SphereGeometry(5,8,8),
+				new THREE.MeshBasicMaterial({color: 0xffff00})
+			);
+		this.player.position.set(0,30,0);
+		this.scene.add(this.player);
+		this.camera.lookAt(this.player.position);
+
+	},
+	movePlayer: function() {
+		// gravity test
+		if (this.player.position.y > -45 ) {
+			this.velocityY -= 0.4;
+		} else {
+
+			this.velocityY = -this.velocityY  ;
+		}
+		this.player.position.y += this.velocityY;
+		this.player.scale.y = 1 + Math.abs(this.velocityY * 0.3);
+	},
+	render: function() {
+		this.movePlayer();
+		this.renderer.render(this.scene, this.camera);
+	},
+	animate: function() {
+		requestAnimationFrame(game.animate);
+		game.render();
+	}
+}
+
+game.init();
